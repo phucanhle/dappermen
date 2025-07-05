f8ef1b3169485062de0f88054ecfdddb83d52e2f
phucanhle <lephucanh2601@gmail.com>
2025-07-05 14:39:58 +0700

    chore: resize categories for mobile

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -8,12 +8,12 @@ interface CategoriesProps {
 
 export default function Categories({ categories, currentCategory, onCategoryChange }: CategoriesProps) {
     return (
-        <div className="flex gap-4 w-full">
+        <div className="flex gap-4 w-full mt-4">
             {categories.map((category) => (
                 <button
                     key={category}
                     onClick={() => onCategoryChange(category)}
-                    className={`cursor-pointer p-2 border  capitalize ${
+                    className={`cursor-pointer p-2 border  capitalize text-sm text-nowrap md:text-base  ${
                         currentCategory === category ? "bg-gray-300" : "bg-white"
                     }`}
                 >
